8e9d6dff40d5dd4cbfdc31b8db407adce635ea1a
rafanaslowski <rafanaslowski@gmail.com>
2025-11-12 16:03:33 -0300

    criação da classe professor, e seus metodos, classe professor ira herdar da classe abstrata Pessoa

--- src/Professor.java
+++ src/Professor.java
@@ -1,6 +1,13 @@
 public class Professor extends Pessoa {
+    public Professor(String nome, String cpf, String email) {
+        super(nome, cpf, email);
+    }
     @Override
     public void exibirInformacoes() {
-
+        // Implementação do método abstrato
+        System.out.println("--- Informações do Professor ---");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("CPF: " + this.cpf);
+        System.out.println("Email: " + this.email);
     }
 }
