c1a42068d0b3118c084f2c217dd05a23d6a644a0
Matheus Gomes da Rosa <matheus_g.rosa@hotmail.com>
2025-11-12 14:58:31 -0300

    Ínicio do projeto, criação classe pessoa e seus metodos

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // teste:
+        System.out.println("Sistema iniciado...");
+
+        // escopo de possiveis instancias:
+        // Aluno aluno = new Aluno("João", "12345678900", "joao@email.com", "BSI");
+        // Professor prof = new Professor("Maria", "98765432100", "maria@email.com", "Computação");
+
+
+
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,26 @@
+public abstract class Pessoa {
+    protected String nome;
+    protected String cpf;
+    protected String email;
+
+    public Pessoa(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public abstract void exibirInformacoes();
+}
