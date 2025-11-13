3f1956cca7047ee71983157d52244138bfa88552
rafanaslowski <rafanaslowski@gmail.com>
2025-11-12 22:09:35 -0300

    criação da classe professor, e seus metodos, classe professor ira herdar da classe abstrata Pessoa

--- src/Professor.java
+++ src/Professor.java
@@ -1,12 +1,31 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Professor extends Pessoa {
+    private String areadeAtuacao;
+    private List<Disciplina> disciplinasMinistradas;
+
+    //construtor
     public Professor(String nome, String cpf, String email) {
         super(nome, cpf, email);
+        this.disciplinasMinistradas = disciplinasMinistradas;;
+        this.areadeAtuacao = areadeAtuacao;
     }
-    private String areadeAtuacao;
+
     public String getAreadeAtuacao() {
         return areadeAtuacao;
-
     }
+    public void setAreadeAtuacao(String areadeAtuacao) {
+        this.areadeAtuacao = areadeAtuacao;
+    }
+
+    public List<Disciplinas> getDisciplinasMinistradas() {
+        return disciplinasMinistradas;
+    }
+    public void setDisciplinasMinistradas(List<Disciplina> disciplinasMinistradas) {
+        this.disciplinasMinistradas = disciplinasMinistradas;
+    }
+
     @Override
     public void exibirInformacoes() {
         System.out.println("Nome: " + nome);
