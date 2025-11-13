5423919b549fc9b0a13f1d2da14fce1795703405
dougblud <prado.d@pucpr.edu.br>
2025-11-12 21:07:36 -0300

    Criação da classe aluno e atributo matricula e metodo exibirInformacoes.

---
+++ src/Aluno.java
@@ -0,0 +1,14 @@
+public class Aluno extends Pessoa {
+    public Aluno(String nome, String cpf, String email)  {
+        super(nome, cpf, email);
+    }
+    private int numeroMatricula;
+
+    @Override
+    public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Email: " + email);
+    }
+
+}
