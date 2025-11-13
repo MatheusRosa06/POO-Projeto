8bac8a40559adcc9c21f672c720b0f4597141e6f
Matheus Gomes da Rosa <matheus_g.rosa@hotmail.com>
2025-11-12 21:41:33 -0300

    Interface criada e adaptação do main para interface.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
 public class Main {
-
     public static void main(String[] args) {
-
-
-        // teste:
-        System.out.println("Sistema iniciado...");
-
-        // instancias:
-        Aluno aluno = new Aluno("João", "12345678900", "joao@email.com");
-        aluno.exibirInformacoes();
-
-        Professor prof = new Professor("Marina", "98765432100", "marina@email.com");
-        prof.exibirInformacoes();
-
-
-
+        Interface cadastro = new Interface();
+        cadastro.setVisible(true);
     }
 }
+
+
