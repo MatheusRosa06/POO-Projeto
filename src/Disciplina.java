a875636ca24d81bd76b20fb6c49e3781957e347e
rafanaslowski <rafanaslowski@gmail.com>
2025-11-17 16:12:16 -0300

    finalização das classes professor e disciplina, com todos os metodos e atributos adicionados, e as classes conversando entre si para associar um professor a um disciplina

--- src/Disciplina.java
+++ src/Disciplina.java
@@ -10,4 +10,30 @@ public class Disciplina {
         this.cargaHoraria = cargaHoraria;
         this.professor = professor;
     }
+    public String getNomeDisciplina() {
+        return nomeDisciplina;
+    }
+    public void setNomeDisciplina(String nomeDisciplina) {
+        this.nomeDisciplina = nomeDisciplina;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public void exibirInformacoesDisciplina(){
+        System.out.println("Disciplina: " + nomeDisciplina);
+        System.out.println("Carga Horaria: " + cargaHoraria);
+        System.out.println("Professor: " + professor.getNome());
+    }
 }
--- src/Professor.java
+++ src/Professor.java
@@ -1,40 +1,51 @@
-import java.util.List;
-import java.util.ArrayList;
+    import java.util.List;
+    import java.util.ArrayList;
+    
+    public class Professor extends Pessoa {
+        private String areadeAtuacao;
+        private List<Disciplina> disciplinasMinistradas;
 
-public class Professor extends Pessoa {
-    private String areadeAtuacao;
-    private List<Disciplina> disciplinasMinistradas;
+        //construtor
+        public Professor(String nome, String cpf, String email, String areadeAtuacao) {
+            super(nome, cpf, email);
+            this.areadeAtuacao = areadeAtuacao;
+            this.disciplinasMinistradas = new ArrayList<>();
+        }
 
-    //construtor
-    public Professor(String nome, String cpf, String email, String areadeAtuacao) {
-        super(nome, cpf, email);
-        this.areadeAtuacao = areadeAtuacao;
-        this.disciplinasMinistradas = new ArrayList<>();
-    }
+        //get areadeAtuacao
+        public String getAreadeAtuacao() {
+            return areadeAtuacao;
+        }
 
-    //get areadeAtuacao
-    public String getAreadeAtuacao() {
-         return areadeAtuacao;
-    }
-    //set areadeAtuacao
-    public void setAreadeAtuacao(String areadeAtuacao) {
-        this.areadeAtuacao = areadeAtuacao;
-    }
+        //set areadeAtuacao
+        public void setAreadeAtuacao(String areadeAtuacao) {
+            this.areadeAtuacao = areadeAtuacao;
+        }
 
-    //get disciplinasMinistradas
-    public List<Disciplina> getDisciplinasMinistradas() {
-        return disciplinasMinistradas;
-    }
-    public void adicionarDisciplina(Disciplina d) {
-        disciplinasMinistradas.add(d);
-    }
+        //get disciplinasMinistradas
+        public List<Disciplina> getDisciplinasMinistradas() {
+            return disciplinasMinistradas;
+        }
 
-    //sobrescrita do metodo abstrato da classe pessoa
-    @Override
-    public void exibirInformacoes() {
-        System.out.println("Nome: " + nome);
-        System.out.println("CPF: " + cpf);
-        System.out.println("Email: " + email);
-        System.out.println("Área de atuação: " + areadeAtuacao);
+        public void adicionarDisciplina(Disciplina d) {
+            disciplinasMinistradas.add(d);
+            d.setProfessor(this);
+        }
+
+        //sobrescrita do metodo abstrato da classe pessoa
+        @Override
+        public void exibirInformacoes() {
+            System.out.println("Nome: " + nome);
+            System.out.println("CPF: " + cpf);
+            System.out.println("Email: " + email);
+            System.out.println("Área de atuação: " + areadeAtuacao);
+            System.out.println("Disciplinas Ministradas:");
+            if (disciplinasMinistradas.isEmpty()) {
+                System.out.println("  Nenhuma disciplina cadastrada.");
+            } else {
+                for (Disciplina d : disciplinasMinistradas) {
+                    System.out.println("  - " + d.getNomeDisciplina());
+                }
+            }
+        }
     }
-}
